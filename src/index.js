32f70b402807ece6fa21bf3003640ee97602b453
VeronikaNemirova <nvn92@tut.by>
2022-02-23 21:07:57 +0300

    feat: created toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const sim = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+const dec = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
 module.exports = function toReadable (number) {
-  
+  if (number === 0) {
+    return 'zero';
+  }
+  n = ('000000000' + number).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+  if (!n) return; 
+  let str = '';
+  str = str + ((n[4] != 0) ? (sim[Number(n[4])] || dec[n[4][0]] + ' ' + sim[n[4][1]]) + 'hundred ' : '');
+  str = str + ((n[5] != 0) ? (sim[Number(n[5])] || dec[n[5][0]] + ' ' + sim[n[5][1]]) : '');
+  return str.trim();
 }
+
